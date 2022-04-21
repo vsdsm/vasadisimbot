bd0f2a809116e0884cedc679c33943b487a68756
vsdsm <vadymbrn@gmail.com>
2022-04-21 23:13:30 +0300

    first add

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
